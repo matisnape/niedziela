5301a7130889d291bbc4ccb4fa692b5e679ff5a4
Anna Anks Nowak <matisnape@wp.pl>
2019-05-26 20:02:40 +0200

    Add method for saving scraped content to file

--- lib/scrape_holidays.rb
+++ lib/scrape_holidays.rb
@@ -11,11 +11,24 @@ module ClosedDays
 
     def run!
       doc = Nokogiri::HTML(open(@url + @year))
+      dates = parse_page(doc)
+    end
+
+    def parse_page(doc)
       items = doc.css('.cbox table tr td:first-of-type')
       items.each_with_object([]) do |item, dates|
         date_string = item.css('a[data-date]').first.attributes["data-date"].value
         dates << Date.parse(date_string)
       end
     end
+
+    def save_parsed_to_file(arr)
+      Dir.mkdir 'scraped' unless File.exist? 'scraped'
+      File.open("scraped/scraped_#{Time.now.to_i}.txt", 'w') do |file|
+        file.write(arr)
+      end
+    end
+
+
   end
 end
