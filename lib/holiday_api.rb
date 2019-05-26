fbf7af419a75600aab648e7717cc127724f737f5
Anna Anks Nowak <matisnape@wp.pl>
2019-05-26 12:37:22 +0200

    Update API key and handling

--- lib/holiday_api.rb
+++ lib/holiday_api.rb
@@ -8,6 +8,7 @@ module ClosedDays
 
     def initialize
       @options = {
+        api_key: ENV.fetch("HOLIDAYAPI"),
         country: 'PL',
         year: Date.today.year
       }
@@ -15,7 +16,11 @@ module ClosedDays
 
     def holidays
       response = self.class.get('/holidays', query: @options)
-      response.parsed_response['holidays'].keys.to_json
+      if response["status"] == 200
+        response.parsed_response['holidays'].keys.to_json
+      else
+        response["status"]
+      end
     end
 
     def holiday_dates
