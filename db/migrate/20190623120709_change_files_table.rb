cddbd7dd90121ad7e63cb885a36845d3195c1f12
Anna Anks Nowak <matisnape@wp.pl>
2019-06-24 07:33:26 +0200

    Fix migration

--- db/migrate/20190623120709_change_files_table.rb
+++ db/migrate/20190623120709_change_files_table.rb
@@ -4,6 +4,5 @@ class ChangeFilesTable < ActiveRecord::Migration[5.2]
       t.remove :content_type
       t.rename :file_contents, :dates_list
     end
-    change_column :scrape_files, :dates_list
   end
 end
